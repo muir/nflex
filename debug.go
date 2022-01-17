8227b54192b32031d816f821d8d042c24f6a7e52
David Muir Sharnoff <muir@idiom.org>
2022-01-17 15:00:56 -0800

    lint

--- debug.go
+++ debug.go
@@ -12,35 +12,13 @@ import (
 	"sync/atomic"
 )
 
-var debugging = true
 var counter int32 = 1000
 
-func debugf(fmt string, args ...interface{}) {
-	log.Printf(fmt, args...)
-}
 func debug(args ...interface{}) {
 	log.Println(args...)
 }
 func debugID() int {
 	return int(atomic.AddInt32(&counter, 1))
-}
-
-func callers(levels int) []string {
-	pc := make([]uintptr, levels)
-	n := runtime.Callers(2, pc)
-	if n == 0 {
-		return nil
-	}
-	frames := runtime.CallersFrames(pc[:n])
-	r := make([]string, 0, n)
-	for {
-		frame, more := frames.Next()
-		r = append(r, fmt.Sprintf("%s:%d %s", filepath.Base(frame.File), frame.Line, filepath.Base(frame.Function)))
-		if !more || len(r) == n {
-			break
-		}
-	}
-	return r
 }
 
 func id(raw Source) string {
