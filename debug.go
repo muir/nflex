9f6a61a8f43dd857236acdc49f133c87bf82e761
David Sharnoff <muir@idiom.org>
2022-04-17 22:50:21 -0700

    bugfix: Keys() didn't work for the top level of a YAML document (#5)
    * - bugfix: Keys() didn't work for the top level of a YAML document
    - improve debugging
    
    * run coverage with debug on

--- debug.go
+++ debug.go
@@ -6,6 +6,7 @@ package nflex
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 	"sync/atomic"
 )
@@ -13,7 +14,15 @@ import (
 var counter int32 = 1000
 
 func debug(args ...interface{}) {
-	log.Println(args...)
+	c := make([]interface{}, len(args))
+	for i, arg := range args {
+		if f, ok := arg.(func() string); ok {
+			c[i] = f()
+		} else {
+			c[i] = arg
+		}
+	}
+	log.Println(c...)
 }
 func debugID() int {
 	return int(atomic.AddInt32(&counter, 1))
@@ -41,3 +50,56 @@ func id(raw Source) string {
 }
 
 var debugCombine = combine
+
+func debugKeys(s Source) string {
+	switch s.Type() {
+	case Undefined:
+		return "=UNDEFINED"
+	case Nil:
+		return "=NIL"
+	case String:
+		str, err := s.GetString()
+		if err != nil {
+			return "=s!" + err.Error() + "!"
+		}
+		return "='" + str + "'"
+	case Float:
+		f, err := s.GetFloat()
+		if err != nil {
+			return "=f!" + err.Error() + "!"
+		}
+		return strconv.FormatFloat(f, 'E', -1, 64)
+	case Int:
+		i, err := s.GetInt()
+		if err != nil {
+			return "=i!" + err.Error() + "!"
+		}
+		return strconv.FormatInt(i, 10)
+	case Bool:
+		b, err := s.GetBool()
+		if err != nil {
+			return "=b!" + err.Error() + "!"
+		}
+		return strconv.FormatBool(b)
+	case Slice:
+		l, err := s.Len()
+		if err != nil {
+			return "slice:!" + err.Error() + "!"
+		}
+		if l == 0 {
+			return "slice:[]"
+		}
+		return "slice:[0.." + strconv.Itoa(l-1) + "]"
+	case Map:
+		k, err := s.Keys()
+		if err != nil {
+			return "keys:!" + err.Error() + "!"
+		}
+		if len(k) == 0 {
+			return "keys:{}"
+		}
+		return `{"` + strings.Join(k, `", "`) + `"}`
+	default:
+		return "=???"
+	}
+}
--- release.go
+++ release.go
@@ -7,3 +7,4 @@ func debugID() int                                 { return 0 }
 func debug(args ...interface{})                    {}
 func id(_ Source) string                           { return "" }
 func debugCombine(_ []string, _ []string) []string { return nil }
+func debugKeys(s Source) string                    { return "" }
